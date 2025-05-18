bc61974c46e66130510cfa07408232cbe81df9f3
AdarkadI <61986140+AdarkadI@users.noreply.github.com>
2025-05-18 19:07:29 -0300

    API que valida o acesso

---
+++ projeto-token-pt2/api-chave-acesso/src/main/java/org/example/LoginController.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/acesso")
+public class LoginController {
+
+    @Autowired
+    private AcessoRepository acessoRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity<Boolean> login(@RequestBody LoginDTO loginDTO) {
+        boolean valido = acessoRepository
+                .findByUsuarioAndSenha(loginDTO.getUsuario(), loginDTO.getSenha())
+                .isPresent();
+        return ResponseEntity.ok(valido);
+    }
+}
